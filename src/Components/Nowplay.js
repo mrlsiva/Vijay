2ec60cd5716c9ac204af97c2f29f79da0ec3b5ac
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2019-10-18 17:17:21 +0530

    image
    image updatation

--- src/Components/Nowplay.js
+++ src/Components/Nowplay.js
@@ -3,7 +3,7 @@ class Nowplay extends Component {
     render() {
         if (this.props.data) {
             var ns = this.props.data.user.map(function (user) {
-                return <div class="movie-slide"> <div class="movie-poster"> <aside> <div> <a href="https://youtu.be/d96cjJhvlMA" data-vbtype="video" class="venobox play"> <i class="material-icons">play_arrow</i> </a> <a href="single-movie.html" class="read-more">read more</a> <span class="date">Released: 7 Mar, 2017</span> </div> </aside>  <a href="#"> <img src={'https://mrlsiva.s3.ap-south-1.amazonaws.com/Vijay/${user.MovieID}.jpg'} alt={user.MovieName} /> </a> </div> <h4 class="no-underline">{user.MovieName}</h4>  <div class="star-rating"> <i class="material-icons">star_rate</i> <i class="material-icons">star_rate</i> <i class="material-icons">star_rate</i> <i class="material-icons">star_rate</i> <i class="material-icons">star_rate</i> </div> </div>
+                return <div class="movie-slide"> <div class="movie-poster"> <aside> <div> <a href="https://youtu.be/d96cjJhvlMA" data-vbtype="video" class="venobox play"> <i class="material-icons">play_arrow</i> </a> <a href="single-movie.html" class="read-more">read more</a> <span class="date">Released: 7 Mar, 2017</span> </div> </aside>  <a > <img src={`https://mrlsiva.s3.ap-south-1.amazonaws.com/Vijay/${user.MovieID}.jpg`} alt={user.MovieName} /> </a> </div> <h4 class="no-underline">{user.MovieName}</h4>  <div class="star-rating"> <i class="material-icons">star_rate</i> <i class="material-icons">star_rate</i> <i class="material-icons">star_rate</i> <i class="material-icons">star_rate</i> <i class="material-icons">star_rate</i> </div> </div>
             })
 
             // var count = 0;
